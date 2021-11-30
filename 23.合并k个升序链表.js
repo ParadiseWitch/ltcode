c1e1fa9e5c3b936208b976b6ed8818189820547d
Maid <zhp92@outlook.com>
2021-11-30 17:41:17 +0800

    23.合并k个升序链表

--- 23.合并k个升序链表.js
+++ 23.合并k个升序链表.js
@@ -73,7 +73,30 @@
  * @return {ListNode}
  */
 var mergeKLists = function(lists) {
-
+  if (lists.length === 0) return null;
+  let result = lists[0];
+  for (let i = 1; i < lists.length; i++) {
+    result = mergeTwoLists(result, lists[i]);
+  }
+  return result;
 };
+var mergeTwoLists = function(l1, l2) {
+  if (l1 == null) return l2;
+  if (l2 == null) return l1;
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+}
+
+// const main = () => {
+//   const lists = [];
+//   const result = mergeKLists(lists);
+//   console.log(result);
+// }
+// main();
 // @lc code=end
 
