6260c99cb5cc5b90dc0fcb6ed3eea03c36928485
Maid <zhp92@outlook.com>
2022-03-11 09:25:09 +0800

    589.n-叉树的前序遍历

---
+++ src/589.n-叉树的前序遍历.js
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=589 lang=javascript
+ *
+ * [589] N 叉树的前序遍历
+ *
+ * https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (74.91%)
+ * Likes:    245
+ * Dislikes: 0
+ * Total Accepted:    121.7K
+ * Total Submissions: 160.9K
+ * Testcase Example:  '[1,null,3,2,4,null,5,6]'
+ *
+ * 给定一个 n 叉树的根节点  root ，返回 其节点值的 前序遍历 。
+ * 
+ * n 叉树 在输入中按层序遍历进行序列化表示，每组子节点由空值 null 分隔（请参见示例）。
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [1,null,3,2,4,null,5,6]
+ * 输出：[1,3,5,6,2,4]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root =
+ * [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+ * 输出：[1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 节点总数在范围 [0, 10^4]内
+ * 0 <= Node.val <= 10^4
+ * n 叉树的高度小于或等于 1000
+ * 
+ * 
+ * 
+ * 
+ * 进阶：递归法很简单，你可以使用迭代法完成此题吗?
+ * 
+ */
+
+// @lc code=start
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function (root) {
+  const ans = []
+  dfs(root, ans);
+  return ans;
+};
+const dfs = (root, ans) => {
+  if (!root) return;
+  ans.push(root.val)
+  for (let child of root.children)
+    dfs(child, ans)
+}
+// @lc code=end
