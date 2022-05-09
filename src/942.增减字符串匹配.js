d5504bee8e2f6d2c14230a30d6d17bab7210e9ae
Maid <zhp92@outlook.com>
2022-05-09 23:49:17 +0800

    942.增减字符串匹配

---
+++ src/942.增减字符串匹配.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=942 lang=javascript
+ *
+ * [942] 增减字符串匹配
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+var diStringMatch = function (s) {
+  let n = s.length, lo = 0, hi = n;
+  const perm = new Array(n + 1).fill(0);
+  for (let i = 0; i < n; ++i) {
+    perm[i] = s[i] === 'I' ? lo++ : hi--;
+  }
+  perm[n] = lo; // 最后剩下一个数，此时 lo == hi
+  return perm;
+};
+// @lc code=end
+
+
+
